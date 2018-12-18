b547c2ff1e594a9e4efa7022219e4a19f6e0fd90
JJ Clements <clementsjj@gmail.com>
2018-12-18 16:01:38 -0500

    Change CORS

--- index.js
+++ index.js
@@ -32,11 +32,14 @@ const port = process.env.PORT || 3000;
 // app.use(cookieParser());
 
 //CORS without the CORS module........
-// app.use(function(req,res,next){
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   })
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //Routes
 app.use('/', indexRouter);
